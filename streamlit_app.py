5b0d648e21afbc9111050dce0a986a69d791c644
Venatus12 <76559407+Venatus12@users.noreply.github.com>
2022-09-29 17:44:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import seaborn as sns
 import plotly.express as px
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+from pillow import Image
 
 
 netflix = pd.read_csv('netflixwinst.csv')
